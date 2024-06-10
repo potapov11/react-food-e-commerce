0282398462f813ce882aaaa874c10f7cf156bc1c
Serezha <volos11223344@yandex.ru>
2024-06-10 20:58:18 +0300

    chapter-2-finished

--- src/components/PizzaBlock.jsx
+++ src/components/PizzaBlock.jsx
@@ -1,6 +1,12 @@
+import React, { useState } from 'react';
+
 export default function PizzaBlock({ title, price }) {
-	console.log('Вызов PizzaBlock');
-	// console.log(props);
+	console.log(React);
+	let [pizzaCount, setPizzaCount] = useState(0);
+
+	function workWithExample() {
+		setPizzaCount(pizzaCount + 1);
+	}
 
 	return (
 		<div className="pizza-block">
@@ -19,7 +25,7 @@ export default function PizzaBlock({ title, price }) {
 			</div>
 			<div className="pizza-block__bottom">
 				<div className="pizza-block__price">от {price} ₽</div>
-				<div className="button button--outline button--add">
+				<button className="button button--outline button--add" onClick={workWithExample}>
 					<svg width="12" height="12" viewBox="0 0 12 12" fill="none" xmlns="http://www.w3.org/2000/svg">
 						<path
 							d="M10.8 4.8H7.2V1.2C7.2 0.5373 6.6627 0 6 0C5.3373 0 4.8 0.5373 4.8 1.2V4.8H1.2C0.5373 4.8 0 5.3373 0 6C0 6.6627 0.5373 7.2 1.2 7.2H4.8V10.8C4.8 11.4627 5.3373 12 6 12C6.6627 12 7.2 11.4627 7.2 10.8V7.2H10.8C11.4627 7.2 12 6.6627 12 6C12 5.3373 11.4627 4.8 10.8 4.8Z"
@@ -27,8 +33,8 @@ export default function PizzaBlock({ title, price }) {
 						/>
 					</svg>
 					<span>Добавить</span>
-					<i>2</i>
-				</div>
+					<i>{pizzaCount}</i>
+				</button>
 			</div>
 		</div>
 	);
