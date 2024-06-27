b9c6156185838c5a1277cd46c09ac9d7580481d2
s.potapov <s.potapov@severnaya.ru>
2024-06-27 18:55:00 +0300

    my com

--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -1,25 +1,25 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
-const categories = ['Все', 'Мясные', 'Вегетарианская', 'Гриль', 'Острые', 'Закрытые'];
+const categories = ["Все", "Мясные", "Вегетарианская", "Гриль", "Острые", "Закрытые"];
 
-export default function Categories() {
-	const [activeIndex, setActiveIndex] = useState(0);
+export default function Categories({ value, onClickCategory }) {
+  // const [activeIndex, setActiveIndex] = useState(0);
 
-	const onClickCategory = (index) => {
-		setActiveIndex(index);
-	};
+  // const onClickCategory = (index) => {
+  //   setActiveIndex(index);
+  // };
 
-	return (
-		<div className="categories">
-			<ul>
-				{categories.map((value, i) => {
-					return (
-						<li key={i} onClick={() => onClickCategory(i)} className={activeIndex === i ? 'active' : ''}>
-							{value}
-						</li>
-					);
-				})}
-			</ul>
-		</div>
-	);
+  return (
+    <div className="categories">
+      <ul>
+        {categories.map((categoryName, i) => {
+          return (
+            <li key={i} onClick={() => onClickCategory(i)} className={value === i ? "active" : ""}>
+              {categoryName}
+            </li>
+          );
+        })}
+      </ul>
+    </div>
+  );
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,27 +1,29 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import Categories from "../components/Categories";
 import Sort from "../components/Sort";
 import PizzaBlock from "../components/PizzaBlock";
 import Skeleton from "../components/PizzaBlock/Skeleton";
 
 export const Home = () => {
+  const [categoryId, setCategoryId] = useState(0);
   const [items, setItems] = React.useState([]);
   const [isLoading, setIsLoading] = React.useState(true);
 
   useEffect(() => {
-    fetch("https://6672bf766ca902ae11b19b43.mockapi.io/items")
+    setIsLoading(true);
+    fetch("https://6672bf766ca902ae11b19b43.mockapi.io/items?category=" + categoryId)
       .then((res) => res.json())
       .then((arr) => {
         setIsLoading(false);
         setItems(arr);
       });
     window.scrollTo(0, 0);
-  }, []);
+  }, [categoryId]);
 
   return (
     <>
       <div className="content__top">
-        <Categories />
+        <Categories value={categoryId} onClickCategory={(i) => setCategoryId(i)} />
         <Sort />
       </div>
       <h2 className="content__title">Все пиццы</h2>
