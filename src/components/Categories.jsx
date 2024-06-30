50f2760cb6a1391f7eab995c1a595e4da441e26e
Serezha <volos11223344@yandex.ru>
2024-06-30 11:48:46 +0300

    добавил сортировку

--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -1,19 +1,17 @@
-import React, { useState } from "react";
-
-const categories = ["Все", "Мясные", "Вегетарианская", "Гриль", "Острые", "Закрытые"];
+const categories = ['Все', 'Мясные', 'Вегетарианская', 'Гриль', 'Острые', 'Закрытые'];
 
 export default function Categories({ value, onChangeCategory }) {
-  return (
-    <div className="categories">
-      <ul>
-        {categories.map((categoryName, i) => {
-          return (
-            <li key={i} onClick={() => onChangeCategory(i)} className={value === i ? "active" : ""}>
-              {categoryName}
-            </li>
-          );
-        })}
-      </ul>
-    </div>
-  );
+	return (
+		<div className="categories">
+			<ul>
+				{categories.map((categoryName, i) => {
+					return (
+						<li key={i} onClick={() => onChangeCategory(i)} className={value === i ? 'active' : ''}>
+							{categoryName}
+						</li>
+					);
+				})}
+			</ul>
+		</div>
+	);
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,42 +1,44 @@
-import React, { useEffect, useState } from "react";
-import Categories from "../components/Categories";
-import Sort from "../components/Sort";
-import PizzaBlock from "../components/PizzaBlock";
-import Skeleton from "../components/PizzaBlock/Skeleton";
+import React, { useEffect, useState } from 'react';
+import Categories from '../components/Categories';
+import Sort from '../components/Sort';
+import PizzaBlock from '../components/PizzaBlock';
+import Skeleton from '../components/PizzaBlock/Skeleton';
 
 export const Home = () => {
-  const [items, setItems] = React.useState([]);
-  const [isLoading, setIsLoading] = React.useState(true);
-  const [categoryId, setCategoryId] = useState(0);
-  const [sortType, setSortType] = React.useState(0);
+	const [items, setItems] = React.useState([]);
+	const [isLoading, setIsLoading] = React.useState(true);
+	const [categoryId, setCategoryId] = useState(0);
+	const [sortType, setSortType] = React.useState({ name: 'популярности', sortProperty: 'rating' });
 
-  console.log(sortType);
+	const order = sortType.sortProperty.includes('-') ? 'asc' : 'desc';
+	const sortBy = sortType.sortProperty.replace('-', '');
+	const category = categoryId > 0 ? `category=${categoryId}` : '';
 
-  useEffect(() => {
-    setIsLoading(true);
-    fetch("https://6672bf766ca902ae11b19b43.mockapi.io/items?category=" + categoryId)
-      .then((res) => res.json())
-      .then((arr) => {
-        setIsLoading(false);
-        setItems(arr);
-      });
-    window.scrollTo(0, 0);
-  }, [categoryId]);
+	useEffect(() => {
+		setIsLoading(true);
+		fetch(`https://6672bf766ca902ae11b19b43.mockapi.io/items?${category}&sortBy=${sortBy}&order=${order}`)
+			.then((res) => res.json())
+			.then((arr) => {
+				setIsLoading(false);
+				setItems(arr);
+			});
+		window.scrollTo(0, 0);
+	}, [categoryId, sortType]);
 
-  return (
-    <>
-      <div className="content__top">
-        <Categories value={categoryId} onChangeCategory={(i) => setCategoryId(i)} />
-        <Sort value={sortType} onChangeSort={(i) => setSortType(i)} />
-      </div>
-      <h2 className="content__title">Все пиццы</h2>
-      <div className="content__items">
-        {isLoading
-          ? [...new Array(6)].map((_, i) => <Skeleton key={i} />)
-          : items.map((obj, index) => <PizzaBlock key={index} title={obj.title} price={obj.price} image={obj.imageUrl} sizes={obj.sizes} types={obj.types} />)}
-      </div>
-    </>
-  );
+	return (
+		<>
+			<div className="content__top">
+				<Categories value={categoryId} onChangeCategory={(i) => setCategoryId(i)} />
+				<Sort value={sortType} onChangeSort={(i) => setSortType(i)} />
+			</div>
+			<h2 className="content__title">Все пиццы</h2>
+			<div className="content__items">
+				{isLoading
+					? [...new Array(6)].map((_, i) => <Skeleton key={i} />)
+					: items.map((obj, index) => <PizzaBlock key={index} title={obj.title} price={obj.price} image={obj.imageUrl} sizes={obj.sizes} types={obj.types} />)}
+			</div>
+		</>
+	);
 };
 
 export default Home;
